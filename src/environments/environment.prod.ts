1646b1bf61f159582d872b8f26ececa56f13fa11
Maxime Beurey <beurey.maxime@gmail.com>
2021-01-30 14:20:23 +0100

    final commit

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig : {
+    apiKey: "AIzaSyAiGqEIm67lXqxIXcXS9LkJATY-G-JrW2o",
+    authDomain: "covid19-maximebeurey.firebaseapp.com",
+    databaseURL: "https://covid19-maximebeurey.firebaseio.com",
+    projectId: "covid19-maximebeurey",
+    storageBucket: "covid19-maximebeurey.appspot.com",
+    messagingSenderId: "364001977951",
+    appId: "1:364001977951:web:18a8310181287248311dea",
+    measurementId: "G-C32CJMBH9B"
+  }
 };
